d6f5e45b9fe76acbe0323937a62509808b8b58c9
nataliagilewska <n.gilewska@tt.com.pl>
2019-06-05 15:16:00 +0200

    Zad. 4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -19,5 +23,32 @@ public class Main {
         secik.add(2L);
         secik.add(3L);
         System.out.println(secik);
+
+        //ZAD.3
+        Map<Integer, String> mapka = new TreeMap<>();
+        mapka.put(1, "1 zmapowany");
+        mapka.put(2, "2 zmapowany");
+        mapka.put(3, "3 zmapowany");
+        System.out.println(mapka);
+
+        //ZAD.4
+        int i;
+        String wpisane;
+        Set<String> secik2 = new HashSet<String>();
+        for (i=1;i<=5;i++)
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Podaj ciag znakow");
+            wpisane = scan.nextLine();
+            if (secik2.equals(wpisane)==false)
+            {
+                secik2.add(wpisane);
+            };
+        }
+        System.out.println(secik2);
+
+        
+
+
     }
 }
